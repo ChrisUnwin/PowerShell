f6e012cc7e232dded0582717cb995805c2126c13
Chris Unwin <Chris.Unwin@red-gate.com>
2020-04-06 10:36:41 +0100

    Blah

--- CopyMaskCopyAzure.ps1
+++ CopyMaskCopyAzure.ps1
@@ -7,8 +7,18 @@ $DBName = 'DMDatabase_Production'
 $TempDBName = 'DM_Prep'
 $TargetDBName = 'DMDatabase_Dev_Kendra'
 
-Get-AzSqlDatabase -ResourceGroupName $ResourceGroupName -ServerName $SourceHopServer
+New-AzSqlDatabaseCopy -ResourceGroupName $ResourceGroupName -ServerName $SourceHopServer -DatabaseName $DBName `
+    -CopyResourceGroupName $ResourceGroupName -CopyServerName $SourceHopServer -CopyDatabaseName $TempDBName
 
-#New-AzSqlDatabaseCopy -ResourceGroupName $ResourceGroupName -ServerName $SourceHopServer -DatabaseName $DBName `
-#    -CopyResourceGroupName $ResourceGroupName -CopyServerName $SourceHopServer -CopyDatabaseName $TempDBName
+Start-Sleep -Seconds 300
 
+& "C:\Program Files\Red Gate\Data Masker for SQL Server 7\DataMaskerCmdLine.exe" PARFILE="C:\Users\chris.unwin\Documents\Data Masker(SqlServer)\Masking Sets\AzureFun.DMSMaskSet"
+
+Start-Sleep -Seconds 300
+
+New-AzSqlDatabaseCopy -ResourceGroupName $ResourceGroupName -ServerName $SourceHopServer -DatabaseName $TempDBName `
+    -CopyResourceGroupName $ResourceGroupName -CopyServerName $TargetServer -CopyDatabaseName $TargetDBName
+
+Start-Sleep -Seconds 300
+
+Remove-AzSqlDatabase -ResourceGroupName $ResourceGroupName -ServerName $SourceHopServer -DatabaseName $TempDBName
