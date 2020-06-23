a7f8b09775c447cbc4ba258d744d928cf3a10fd8
Chris Unwin <Chris.Unwin@red-gate.com>
2020-06-23 07:23:13 +0100

    Updated to add copy classifications to target

--- Demos/Redgate Demos/CatalogChecksAtDeployment.ps1
+++ Demos/Redgate Demos/CatalogChecksAtDeployment.ps1
@@ -1,7 +1,9 @@
 # Data Catalog Connection Variables
 $authToken = "NzIwODk5NDkyMDg1MjM1NzEyOjE5NGJhNDA1LWM0ODctNGVmOS1iMGZkLTMxOTAzNzliZjdjZA=="
-$serverUrl = "http://pse-lt-chrisu:15156" 
-$instanceName = 'PSE-LT-CHRISU\'
+$serverUrl = "http://pse-lt-chrisu:15156"
+$WebRequestURI = "http://pse-lt-chrisu:15156/powershell"
+$SourceInstanceName = 'PSE-LT-CHRISU\'
+$TargetInstanceName = 'PSE-LT-CHRISU\'
 $SourceDatabaseName = 'VoiceOfTheDBA_Acceptance'
 $TargetdatabaseName = 'VoiceOfTheDBA_Production'
 
@@ -10,7 +12,6 @@ $TargetdatabaseName = 'VoiceOfTheDBA_Production'
 $JSONFile = Get-Content -Raw -Path "C:\Users\chris.unwin\Desktop\Changes.json" | ConvertFrom-Json
 
 # Check for any changes and assert if they are table changes
-If ($JSONFile.ObjectCounts.Created -gt 0 -or $JSONFile.ObjectCounts.Modified -gt 0) {
    
    $ModifiedTables = $JSONFile.modifiedObjects.atSource.objectName | Where-Object {$JSONFile.modifiedObjects.objectType -eq "Table"}
    $ModifiedOwners = $JSONFile.modifiedObjects.atSource.owner | Where-Object {$JSONFile.modifiedObjects.objectType -eq "Table"}
@@ -18,41 +19,90 @@ If ($JSONFile.ObjectCounts.Created -gt 0 -or $JSONFile.ObjectCounts.Modified -gt
    $CreatedOwners = $JSONFile.createdObjects.atSource.objectOwner | Where-Object {$JSONFile.createdObjects.objectType -eq "Table"}
 
    # Connect and get existing columns from Data Catalog
-   Invoke-WebRequest -Uri "http://pse-lt-chrisu:15156/powershell" -OutFile 'data-catalog.psm1' -Headers @{"Authorization"="Bearer $authToken"}
+   Invoke-WebRequest -Uri $WebRequestURI -OutFile 'data-catalog.psm1' -Headers @{"Authorization"="Bearer $authToken"}
    Import-Module .\data-catalog.psm1 -Force
    Connect-SqlDataCatalog -ServerUrl $serverUrl -AuthToken $authToken 
-   $CatalogSourceColumns = Get-ClassificationColumn -InstanceName $instanceName -DatabaseName $SourceDatabaseName 
-   $CatalogTargetColumns = Get-ClassificationColumn -InstanceName $instanceName -DatabaseName $TargetDatabaseName 
-   
-   #Get the Columns / Tables that have _changed_ and check in Data Catalog
-    If ($ModifiedTables.count -gt 1){
+
+   Start-ClassificationDatabaseScan -FullyQualifiedInstanceName $SourceInstanceName -DatabaseName $SourceDatabaseName | Wait-SqlDataCatalogOperation
+   Start-ClassificationDatabaseScan -FullyQualifiedInstanceName $TargetInstanceName -DatabaseName $TargetDatabaseName | Wait-SqlDataCatalogOperation
+
+   $SourceColumnsMissingClass = @()
+   $TargetColumnsMissingClass = @()
+   $ColumnsClassInSourceNotInTarget = @()
+   $ColumnsNotClassInSourceNotInTarget = @()
+   $CatalogSourceColumns = Get-ClassificationColumn -InstanceName $SourceInstanceName -DatabaseName $SourceDatabaseName 
+   $CatalogTargetColumns = Get-ClassificationColumn -InstanceName $TargetInstanceName -DatabaseName $TargetDatabaseName 
+
+   foreach ($column in $CatalogTargetColumns) {
+        If ($column.sensitivityLabel -and $column.tags )  { Continue }
+        Else { $TargetColumnsMissingClass += ("$($column.schemaName).$($column.tableName).$($column.columnName)") }
+    }
+    foreach ($column in $CatalogSourceColumns) {
+        If ($column.sensitivityLabel -or $column.tags )  {
+            $Mix = ("$($column.schemaName).$($column.tableName).$($column.columnName)")  
+            $ColumnsClassInSourceNotInTarget +=  $Mix | Where-Object {$Mix -in $TargetColumnsMissingClass}
+        }
+        Else { 
+            $Mix = ("$($column.schemaName).$($column.tableName).$($column.columnName)") 
+            $SourceColumnsMissingClass += $Mix
+            $ColumnsNotClassInSourceNotInTarget +=  $Mix | Where-Object {$Mix -notin $TargetColumnsMissingClass}
+        }
+    }
+
+    $ColumnsClassInSourceNotInTarget2 = $ColumnsClassInSourceNotInTarget | Select-Object -Unique
+    $ColumnsNotClassInSourceNotInTarget2 = $ColumnsNotClassInSourceNotInTarget | Select-Object -Unique
+    
+   #Get the Columns / Tables that have _changed_ 
+   $ModTab = @() 
+   If ($ModifiedTables.count -gt 1){
         $i = 0
         While ($i -lt $ModifiedTables.count) {
-            $ModTab = $ModifiedOwners[$i] + "." + $ModifiedTables[$i]
-
-            Write-Host "Table $ModTab was modified."  
+            $ModTab += $ModifiedOwners[$i] + "." + $ModifiedTables[$i]
+            Write-Host "(Information) Table $($ModifiedOwners[$i]).$($ModifiedTables[$i]) was modified in this deployment."  -BackgroundColor green -ForegroundColor black
             $i = $i + 1    
             }
     }
     Elseif ($ModifiedTables.count -eq 1) {
-        Write-Host "$($ModifiedOwners).$($ModifiedTables) was modified."
+        $ModTab += $ModifiedOwners+"."+$ModifiedTables
+        Write-Host "$ModTab was modified in this deployment." -BackgroundColor green -ForegroundColor black
     }
     Else {
-        Write-Host "No tables were created."
+        Write-Host "No tables were modified in this deployment." -BackgroundColor green -ForegroundColor black
     }
 
-    #Get the Columns / Tables that have been added and check in Data Catalog
+    #Get the Columns / Tables that have been added 
     If ($CreatedTables.count -gt 1){
         $i = 0
         While ($i -lt $CreatedTables.count) {
-            Write-Host "$($CreatedOwners[$i]).$($CreatedTables[$i]) was created."  
+            Write-Host "$($CreatedOwners[$i]).$($CreatedTables[$i]) was created in this deployment."  -BackgroundColor green -ForegroundColor black
             $i = $i + 1    
             }
     }
     Elseif ($CreatedTables.count -eq 1) {
-        Write-Host "$($CreatedOwners).$($CreatedTables) was created."
+        Write-Host "Table $($CreatedOwners).$($CreatedTables) was created." -BackgroundColor green -ForegroundColor black
     }
     Else {
-        Write-Host "No tables were created."
+        Write-Host "No tables were created in this deployment." -BackgroundColor green -ForegroundColor black
     }
-} Else { Exit }
+
+    #Action: If modified tables contain columns only classified in source, copy classifications up
+
+    If ($ColumnsClassInSourceNotInTarget2.count -gt 0) {
+        Write-Host "$($ColumnsClassInSourceNotInTarget2.count) column(s) with classifications were discovered on $($SourceDatabaseName) that are not classified in $($TargetDatabaseName):"`n"$ColumnsClassInSourceNotInTarget2" -BackgroundColor green -ForegroundColor black
+        Write-Host "(Action) Copying classifications from $($SourceInstanceName)$($SourceDatabaseName) to $($TargetInstanceName)$($TargetDatabaseName)" -ForegroundColor Black -BackgroundColor Green
+        Copy-Classification -sourceInstanceName $SourceInstanceName -sourceDatabaseName $SourceDatabaseName -destinationInstanceName $TargetInstanceName -destinationDatabaseName $TargetdatabaseName | Wait-SqlDataCatalogOperation
+    }
+
+    # Action: If Columns exist in source that have not been classified then tell them to classify them : Warning
+
+    If ($ColumnsNotClassInSourceNotInTarget2.count -gt 0) {
+        Write-Host "(Warning) The following columns have been discovered on $($SourceDatabaseName) that do not have classifications and are not present in $($TargetDatabaseName):"`n"$ColumnsNotClassInSourceNotInTarget2 "`n"You should classify these columns prior to deployment." -BackgroundColor yellow -ForegroundColor black 
+    }
+    Else { Write-Host "No columns were discovered on $($SourceDatabaseName) that do not have classifications and are not present in $($TargetDatabaseName)." -BackgroundColor green -ForegroundColor black }
+
+    # Action: If columns exist on the target that have not been classified tell them to classify them : Alert
+
+    If ($TargetColumnsMissingClass.count -gt 0) {
+        Write-Host "(Alert) The following columns have been discovered on $($TargetDatabaseName) that require classification:"`n"$TargetColumnsMissingClass "`n"You should classify these columns in $($SourceInstanceName)$($SourceDatabaseName) prior to the next deployment." -BackgroundColor red -ForegroundColor white 
+    }
+    Else { Write-Host "No columns were discovered on $($TargetDatabaseName) that require classification." -BackgroundColor green -ForegroundColor black }
