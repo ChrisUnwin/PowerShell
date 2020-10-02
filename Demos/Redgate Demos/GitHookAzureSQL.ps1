71281324974d19339144cb68f11a568c9cb3a821
Chris Unwin <Chris.Unwin@red-gate.com>
2020-10-02 08:24:42 +0100

    Making general and adding comments

--- Demos/Redgate Demos/GitHookAzureSQL.ps1
+++ Demos/Redgate Demos/GitHookAzureSQL.ps1
@@ -1,18 +1,32 @@
-$ResourceGroupName = 'DMDb' #Azure Resource Group Name
-$ServerName = 'dmnonproduction'
-$DBName = 'DMDatabase_Dev' #Active Development DB
-$SubscriptionID = 'a36be632-e20c-48fd-8af0-ba5b2c623951'
+#############################################################################
+# Script for easily branch switching between different Azure SQL Databases ##
+#############################################################################
+
+## Update the below values 
+
+$ResourceGroupName = 'YourResourceGroup' #Azure Resource Group Name
+$ServerName = 'YourServer' #Your non prod Azure Server name
+$DBName = 'YourDatabase' #Active Development DB
+$SubscriptionID = 'INSERT SUB ID' #Your subscription ID
 
 #############################################################################
+
+## If you want a different method of authentication then change the below
 
 Connect-AzAccount #Connect to Azure Account and set subscription context
 Set-AzContext -SubscriptionId $SubscriptionID
 
 #############################################################################
 
+## GitHook Script
+
+#Check if Git is installed locally first
+
 if ($null -eq (Get-Command "git" -ErrorAction SilentlyContinue)) {
     throw "This script requires git to be installed and available on PATH"
 }
+
+#Fetch the To and From Branch for naming purposes
 
 $reflogResult = git reflog
 $lastReflog = $reflogResult[0]
@@ -29,6 +43,8 @@ if ($fromBranch -eq $toBranch) {
 Write-Host "Provisioning database ${DBName} for branch ${toBranch}..."
 Write-Host "Switched from branch ${fromBranch} to branch ${toBranch}"
 
+#Check if a golden copy exists and create one if it doesn't
+
 $GoldenDBName = $DBName+"_Golden"
 $ToBranchDB = $DBName+"_"+$toBranch
 $FromBranchDB = $DBName+"_"+$fromBranch
@@ -43,13 +59,15 @@ if ($ExistingDBs.DatabaseName -NotContains $GoldenDBName) {
 
 }
 
+#If you're switching to a DB that already exists, straight up rename, else create a new copy to work from
+
 if ($ExistingDBs.DatabaseName -NotContains $ToBranchDB) {
 
     Write-Host "$ToBranchDB not found, renaming and provisioning new database copy..."
 
     Set-AzSqlDatabase -DatabaseName $DBName -NewName $FromBranchDB -ServerName $ServerName -ResourceGroupName $ResourceGroupName 
 
-    Write-Host "$FromBranchDB created, creating new master copy for branch $toBranch..."
+    Write-Host "$FromBranchDB created, creating new live dev copy for branch $toBranch..."
 
     New-AzSqlDatabaseCopy -ResourceGroupName $ResourceGroupName -ServerName $ServerName -DatabaseName $GoldenDBName `
     -CopyResourceGroupName $ResourceGroupName -CopyServerName $ServerName -CopyDatabaseName $DBName
