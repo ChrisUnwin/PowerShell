cd82782e3b95237beb6e5a86426950ded06d53a8
Chris Unwin <Chris.Unwin@red-gate.com>
2021-08-05 11:59:16 +0100

    New Catalog Scripts

---
+++ Demos/Redgate Demos/CatalogSimplePipelineChecks/CatalogPipelineCheckPreDeployment.ps1
@@ -0,0 +1,29 @@
+#### Change this section only ###
+
+$CatalogServer="" # The lcoation of your catalog server, ending on :15156 
+$authToken="" # Your Data Catalog Auth token from the Settings page
+$instanceName = ""
+$databaseName = ""
+
+#### Change this section only ###
+
+If ($CatalogServer.Substring($CatalogServer.length - 1) -eq "/") {$fullUri = $CatalogServer + "powershell"} 
+Else {$fullUri = $CatalogServer + "/powershell"}
+Invoke-WebRequest -Uri $fullUri -OutFile 'data-catalog.psm1' -Headers @{"Authorization"="Bearer $authToken"}
+Import-Module .\data-catalog.psm1 -Force
+Connect-SqlDataCatalog -ServerUrl $CatalogServer -AuthToken $authToken 
+
+$UnclassifiedColumns = Get-ClassificationColumn -InstanceName $instanceName -DatabaseName $databaseName | Where-Object {$_.tags.count -eq 0}
+$countOfColumns = $UnclassifiedColumns.count 
+
+If ($countOfColumns -eq 0) {
+    "You have no columns pending classification on $instanceName/$databaseName"
+}
+Else {
+
+    "You have $countOfColumns Columns on $instanceName/$databaseName pending classification, please do so before promoting the deployment:"
+    $UnclassifiedColumns | Foreach-Object {`
+        $_.schemaName + '.' + $_.tableName + '.' + $_.columnName
+    }
+    Exit 1
+}
---
+++ Demos/Redgate Demos/CatalogSimplePipelineChecks/CatalogPromoteClassificationsPostDeployment.ps1
@@ -0,0 +1,21 @@
+#### Change this section only ###
+
+$CatalogServer="" # The lcoation of your catalog server, ending on :15156 
+$authToken="" # Your Data Catalog Auth token from the Settings page
+$sourceInstanceName = ""
+$targetInstanceName = ""
+$sourceDatabaseName = ""
+$targetDatabaseName = ""
+
+#### Change this section only ###
+
+If ($CatalogServer.Substring($CatalogServer.length - 1) -eq "/") {$fullUri = $CatalogServer + "powershell"} 
+Else {$fullUri = $CatalogServer + "/powershell"}
+Invoke-WebRequest -Uri $fullUri -OutFile 'data-catalog.psm1' -Headers @{"Authorization"="Bearer $authToken"}
+Import-Module .\data-catalog.psm1 -Force
+Connect-SqlDataCatalog -ServerUrl $CatalogServer -AuthToken $authToken 
+
+Start-ClassificationDatabaseScan -FullyQualifiedInstanceName $targetInstanceName -DatabaseName $targetDatabaseName
+
+Copy-Classification -SourceInstanceName $sourceInstanceName -DestinationInstanceName $targetInstanceName `
+ -SourceDatabaseName $sourceDatabaseName -DestinationDatabaseName $targetDatabaseName
